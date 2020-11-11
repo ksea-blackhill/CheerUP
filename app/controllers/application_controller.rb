eb5168bfd310b93c9152b7f026419525e261e922
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-11-11 19:38:39 +0900

    ユーザー管理機能の詰め段階

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :login_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first, 
+      :kana_last, :nickname, :prefecture_id, :age_id, :gender_id])
+  end
+
+  #def login_permitted_parameters
+    #devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+  #end
 end
