06b6a3b033e900e8d3c88835f8c1e5503f9399fa
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-11-18 18:20:16 +0900

    userモデルとのアソシエーション

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :genre
+  belongs_to_active_hash :member
+
+  belongs_to :user
 end
