c6553857ef2bdd9ba36d0b6bfd68eb7567d12cf4
ksea-blackhill <71749222+ksea-blackhill@users.noreply.github.com>
2020-11-30 10:53:00 +0900

    Formオブジェクトパターンの単体テスト実施

---
+++ spec/factories/artist_forms.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :artist_form do
+    price { 1000 }
+    comment { 'sample,sample,sample,sample,sample'}
+    token { 'tok_abcdefghijk00000000000000000' }
+  end
+end
